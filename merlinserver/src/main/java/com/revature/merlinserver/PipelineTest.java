60ddf1d23270dbd9c7d16ed40189f2c2c0dae756
Evan West <evan-west@hotmail.com>
2017-12-15 16:18:22 -0500

    Initial Maven Project

---
+++ merlinserver/src/main/java/com/revature/merlinserver/PipelineTest.java
@@ -0,0 +1,10 @@
+package com.revature.merlinserver;
+
+public class PipelineTest {
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Hello World! " + i);
+		}
+	}
+}
+
