60092b7533231c78f2cbf6b3920f783afbbf096e
Evan West <evan-west@hotmail.com>
2017-12-15 23:13:29 -0500

    Another Pipeline Test: Change print statements

--- merlinserver/src/main/java/com/revature/merlinserver/PipelineTest.java
+++ merlinserver/src/main/java/com/revature/merlinserver/PipelineTest.java
@@ -5,8 +5,7 @@ public class PipelineTest {
 		for(int i = 0; i < 10; i++) {
 			System.out.println("Hello World! " + i);
 		}
-		System.out.println("A Final Hello!");
-		System.out.println("Another Final Hello!");
+		System.out.println("A Changed Final Hello!");
 	}
 }
 
