d386b279d5cf03833379d5412436a90adbbd2af1
antony lulciuc <antony.lulciuc@gmail.com>
2017-12-16 21:32:26 -0500

    Created CodeList bean

--- merlinserver/src/main/java/com/revature/merlinserver/beans/MagicalUser.java
+++ merlinserver/src/main/java/com/revature/merlinserver/beans/MagicalUser.java
@@ -9,7 +9,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="User")
+@Table(name="MagicalUser")
 public class MagicalUser {
 	@Id
 	@Column(name="USER_ID")
