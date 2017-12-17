d6007c3886791ce4ddad7c030ea84574071670ec
antony lulciuc <antony.lulciuc@gmail.com>
2017-12-16 20:23:42 -0500

    added junit 4 to mvn pom -Created JUnit ConnectionTest class with stub

---
+++ merlinserver/src/test/java/com/revature/merlinservertests/ConnectionTest.java
@@ -0,0 +1,17 @@
+package com.revature.merlinservertests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Determines if Hibernate can get connection from remote database
+ * @author Luie
+ */
+public class ConnectionTest {
+
+	@Test
+	public void shouldGetNonNullHibernateSession() {
+		fail("Not yet implemented");
+	}
+}
