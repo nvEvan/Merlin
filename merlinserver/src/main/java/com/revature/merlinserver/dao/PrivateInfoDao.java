d7f020579615e81919873c9cea0fff2dc764541f
Alex Peterson <amp1993@gmail.com>
2017-12-22 13:40:11 -0500

    privateinfo dao

---
+++ merlinserver/src/main/java/com/revature/merlinserver/dao/PrivateInfoDao.java
@@ -0,0 +1,13 @@
+package com.revature.merlinserver.dao;
+
+import com.revature.merlinserver.beans.PrivateUserInfo;
+
+public class PrivateInfoDao extends MerlinSessionDao<PrivateUserInfo> {
+	
+	public void update(PrivateUserInfo info) {
+		
+		
+		
+	}
+
+}
