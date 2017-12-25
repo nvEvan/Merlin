72641d7e01cb1e2f3f8e31d1519d5622dccec6c7
Alex Peterson <amp1993@gmail.com>
2017-12-25 18:01:22 -0500

    privateinfo test

--- merlinserver/src/test/java/com/revature/merlintest/junit/PrivateInfoDaoTest.java
+++ merlinserver/src/test/java/com/revature/merlintest/junit/PrivateInfoDaoTest.java
@@ -1,20 +1,17 @@
 package com.revature.merlintest.junit;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.revature.merlinserver.dao.PrivateInfoDao;
 
 public class PrivateInfoDaoTest {
 	
 	@Test
-	public void shouldLoadAllUsers() {
-		PrivateInfoDao dao = new PrivateInfoDao();
+	public void getInfoByUserTest() {
 		
-		assertTrue(dao.open());
-//		assertNotNull(dao.update());
-		dao.close();
+		
+		
+		
+		
 	}
 }
