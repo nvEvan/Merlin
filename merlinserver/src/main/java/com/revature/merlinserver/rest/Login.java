10348edabb6c381c7ea32ebc51655d7e425750c1
Nasir Alauddin <nalaudd@yahoo.com>
2017-12-26 10:13:25 -0500

    Rest file updated

--- merlinserver/src/main/java/com/revature/merlinserver/rest/Login.java
+++ merlinserver/src/main/java/com/revature/merlinserver/rest/Login.java
@@ -1,12 +1,16 @@
 package com.revature.merlinserver.rest;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.revature.merlinserver.beans.MagicalUser;
+import com.revature.merlinserver.dao.MagicalUserDao;
 
 /**
  * TODO : do login validation and other stuff
@@ -22,4 +26,21 @@ public class Login {
 	public String validate(MagicalUser user) {
 		return "stuff = " + user.getUsername() + " " + user.getPassword();
 	}
+	
+	//Gets all users 
+	@GET
+	@Path("/MagicalUser/get/all")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<MagicalUser> getMagicalUsers(){
+		MagicalUserDao dao = new MagicalUserDao();
+		dao.open();
+		List<MagicalUser> users = dao.loadAll();
+		System.out.println(users);
+		for(MagicalUser m : users){
+			System.out.println(m.getUsername());
+		}
+		//dao.close();
+		return users;
+	}
 }
+
