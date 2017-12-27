58b85448711cf94043b9dccf953fe59f225516f6
Nasir Alauddin <nalaudd@yahoo.com>
2017-12-27 18:13:35 -0500

    Updated rest file

--- merlinserver/src/main/java/com/revature/merlinserver/rest/Login.java
+++ merlinserver/src/main/java/com/revature/merlinserver/rest/Login.java
@@ -6,11 +6,14 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.revature.merlinserver.beans.MagicalUser;
+import com.revature.merlinserver.beans.PrivateUserInfo;
 import com.revature.merlinserver.dao.MagicalUserDao;
+import com.revature.merlinserver.dao.PrivateInfoDao;
 
 /**
  * TODO : do login validation and other stuff
@@ -18,6 +21,7 @@ import com.revature.merlinserver.dao.MagicalUserDao;
  */
 @Path("/login")
 public class Login {
+	
 	// TODO : do login stuff
 	@POST
 	@Path("/validate")
@@ -28,19 +32,57 @@ public class Login {
 	}
 	
 	//Gets all users 
-	@GET
+	@POST
 	@Path("/MagicalUser")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<MagicalUser> getMagicalUsers(){
 		MagicalUserDao dao = new MagicalUserDao();
 		dao.open();
 		List<MagicalUser> users = dao.loadAll();
-		System.out.println(users);
-		for(MagicalUser m : users){
-			System.out.println(m.getUsername());
-		}
 		dao.close();
 		return users;
+	}
+	
+	//Get explicit user
+	@POST
+	@Path("/MagicalUser/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public MagicalUser getMagicalUser(@PathParam("username") String username){
+		MagicalUserDao dao = new MagicalUserDao();
+		dao.open();
+		List<MagicalUser> users = dao.loadAll();
+		dao.close();
+		for(MagicalUser m : users){
+			if(username.equals(m.getUsername())){
+				return m;
+			}
+		}
+		return null;
+	}
+	
+	//Get explicit user info
+	@POST
+	@Path("/MagicalUser/info/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public PrivateUserInfo getPublicInfo(@PathParam("username") String username){
+		PrivateInfoDao infoDao = new PrivateInfoDao();
+		MagicalUserDao userDao = new MagicalUserDao();
+		PrivateUserInfo info = null;
+		
+		userDao.open();
+		List<MagicalUser> users = userDao.loadAll();
+		userDao.close();
+		
+		for(MagicalUser m : users){
+			if(username.equals(m.getUsername())){
+				infoDao.open();
+				info = infoDao.getPrivateInfoByUser(m);
+				infoDao.close();
+				return info;
+			}
+		}
+		infoDao.close();
+		return null;
 	}
 }
 
