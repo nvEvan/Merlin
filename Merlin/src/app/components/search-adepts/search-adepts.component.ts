dbf742e9b8cbc9e79a1591bb36f83b01f2c93a51
Evan West <evan-west@hotmail.com>
2017-12-28 18:50:40 -0500

    Basic ngFor dispaly of hardcoded list.
    Followed tutorial listed in code

--- Merlin/src/app/components/search-adepts/search-adepts.component.ts
+++ Merlin/src/app/components/search-adepts/search-adepts.component.ts
@@ -1,14 +1,37 @@
+//ngFor tutorial: https://blog.angular-university.io/angular-2-ngfor/
+
 import { Component } from '@angular/core'; 
 import { Http, Response } from '@angular/http';
 
 @Component({
     selector:'app-search-adepts',
-    templateUrl: './search-adepts.component.html', 
-    styleUrls: ['./search-adepts.component.css']
+    template: `
+    <table>
+        <thead>
+            <th>Name</th>
+            <th>Index</th>
+        </thead>
+        <tbody>
+            <tr *ngFor="let hero of heroes">
+                <td>{{hero.name}}</td>
+            </tr>
+        </tbody>
+    </table>
+`
+    // templateUrl: './search-adepts.component.html', 
+    // styleUrls: ['./search-adepts.component.css']
 })
 
 export class SearchAdeptsComponent {
-    constructor(private http: Http) {}
+    HEROES = [
+        {id: 1, name:'Superman'},
+        {id: 2, name:'Batman'},
+        {id: 5, name:'BatGirl'},
+        {id: 3, name:'Robin'},
+        {id: 4, name:'Flash'}
+    ];
+    heroes = this.HEROES;
+    constructor(private http: Http){};
 
     doSomething() {
         this.http.get("http://localhost:8080/merlinserver/rest/fetch/adepts/all")
@@ -18,7 +41,5 @@ export class SearchAdeptsComponent {
                 console.log(Adepts);
             }
         )
-    }
-
-
+    };
 }
