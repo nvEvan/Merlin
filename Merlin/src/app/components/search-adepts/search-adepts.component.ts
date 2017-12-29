e67c3ac7992245c609eda7dd737b85f88b3eba30
Evan West <evan-west@hotmail.com>
2017-12-29 15:38:17 -0500

    fixed merge conflicts

--- Merlin/src/app/components/search-adepts/search-adepts.component.ts
+++ Merlin/src/app/components/search-adepts/search-adepts.component.ts
@@ -10,6 +10,9 @@ import { Http, Response } from '@angular/http';
 })
 
 export class SearchAdeptsComponent {
+    heroes : any;
+    Adepts : any;
+    
     HEROES = [
         {id: 1, name:'Superman'},
         {id: 2, name:'Batman'},
@@ -17,16 +20,31 @@ export class SearchAdeptsComponent {
         {id: 3, name:'Robin'},
         {id: 4, name:'Flash'}
     ];
-    heroes = this.HEROES;
-    constructor(private http: Http){};
-
-    doSomething() {
+    
+    constructor(private http: Http){
         this.http.get("http://localhost:8080/merlinserver/rest/fetch/adepts/all")
         .subscribe(
             (res: Response) => {
-                const Adepts = res.json();
-                console.log(Adepts);
+                console.log(res);
+                console.log("after res._body")
+                const Adepts = res.json;
+                // this.Adepts = res.json;
+                
+                this.heroes = this.HEROES;
+                console.log(typeof(this.Adepts))
+                console.log(this.Adepts);
+                console.log(this.heroes);
             }
         )
     };
+
+    // doSomething() {
+    //     this.http.get("http://localhost:8080/merlinserver/rest/fetch/adepts/all")
+    //     .subscribe(
+    //         (res: Response) => {
+    //             const Adepts = res.json();
+    //             console.log(Adepts);
+    //         }
+    //     )
+    // };
 }
