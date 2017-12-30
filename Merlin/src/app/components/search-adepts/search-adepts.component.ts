c5dd21e885552295951560d80ae2116299e76f12
Evan West <evan-west@hotmail.com>
2017-12-29 20:51:03 -0500

    Fetch a list of users from the database and log them to the console

--- Merlin/src/app/components/search-adepts/search-adepts.component.ts
+++ Merlin/src/app/components/search-adepts/search-adepts.component.ts
@@ -10,41 +10,16 @@ import { Http, Response } from '@angular/http';
 })
 
 export class SearchAdeptsComponent {
-    heroes : any;
     Adepts : any;
     
-    HEROES = [
-        {id: 1, name:'Superman'},
-        {id: 2, name:'Batman'},
-        {id: 5, name:'BatGirl'},
-        {id: 3, name:'Robin'},
-        {id: 4, name:'Flash'}
-    ];
-    
     constructor(private http: Http){
-        this.http.get("http://localhost:8080/merlinserver/rest/fetch/adepts/all")
+        this.http.get("http://localhost:8085/merlinserver/rest/fetch/adepts/testuser")
         .subscribe(
             (res: Response) => {
-                console.log(res);
-                console.log("after res._body")
-                const Adepts = res.json;
-                // this.Adepts = res.json;
-                
-                this.heroes = this.HEROES;
-                console.log(typeof(this.Adepts))
+                console.log("Fetching Adepts");
+                this.Adepts = res.json();
                 console.log(this.Adepts);
-                console.log(this.heroes);
             }
         )
     };
-
-    // doSomething() {
-    //     this.http.get("http://localhost:8080/merlinserver/rest/fetch/adepts/all")
-    //     .subscribe(
-    //         (res: Response) => {
-    //             const Adepts = res.json();
-    //             console.log(Adepts);
-    //         }
-    //     )
-    // };
 }
