cf82206fe54f9640682414d6fbc81f7efc1784bd
Alex Peterson <amp1993@gmail.com>
2017-12-30 16:10:12 -0500

    working on adept file upload

--- Merlin/src/app/components/register/register.component.ts
+++ Merlin/src/app/components/register/register.component.ts
@@ -10,6 +10,8 @@ import { Observable } from "rxjs/Observable";
 import { Router } from '@angular/router';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { CodeListService } from '../../services/codelist/codelist.service';
+import { MagicalFileUpload } from '../../models/magical-file-upload.model';
+import { RequestOptions } from '@angular/http/src/base_request_options';
 
 /**
  * Component for user registration
@@ -27,16 +29,19 @@ import { CodeListService } from '../../services/codelist/codelist.service';
 export class RegisterComponent implements OnInit {
   @ViewChild("citydiv") citySelect: ElementRef
   @ViewChild("statediv") stateSelect: ElementRef
+  @ViewChild('fileUpload') fileUpload;
 
   private magicalUser: MagicalUser
   private privateUserInfo: PrivateUserInfo
   private apprenticeData: UserPrivateData
+  private magicalFileUpload: MagicalFileUpload
   confirmPassword: string = ""
 
   showPasswordAlert: boolean = false
   showUsernameAlert: boolean = false
-  showStateCityAlert : boolean = false
-  fieldsAreBlank : boolean = false
+  showStateCityAlert: boolean = false
+  fieldsAreBlank: boolean = false
+  adeptAccount: boolean = false
 
   roles: CodeList[]
 
@@ -55,26 +60,13 @@ export class RegisterComponent implements OnInit {
     private codeListService: CodeListService) {
     this.magicalUser = new MagicalUser()
     this.privateUserInfo = new PrivateUserInfo()
+    this.magicalFileUpload = new MagicalFileUpload()
   }
 
   /**
    * Attempt to register the new user.
    */
   register(): void {
-    let city = this.citySelect.nativeElement.children[0].children[0].value
-    let state = this.stateSelect.nativeElement.children[0].children[0].value
-
-    if (city == null || state == null)
-      return
-
-    //check if the user inputted a valid state/city combination
-    this.codeListService.getStateCityCode(state, city).subscribe(
-      data => {
-        this.privateUserInfo.stateCity = data
-        this.showStateCityAlert = data == null
-      }
-    )
-
     //check the user's info is ready to be registered
     if (this.allFieldsAreEntered() && !this.showUsernameAlert && !this.showPasswordAlert) {
       if (this.privateUserInfo.role.value.toString() == "APPRENTICE") { //register an apprentice account
@@ -89,6 +81,8 @@ export class RegisterComponent implements OnInit {
         this.registerService.registerApprentice(this.apprenticeData);
 
         this.router.navigate(['home'])
+      } else {
+        //adept registration
       }
     }
   }
@@ -112,9 +106,6 @@ export class RegisterComponent implements OnInit {
    * Check that the user's passwords match
    */
   passwordsMatch(): void {
-    if (this.magicalUser.password == null || this.magicalUser.password.length < 1)
-      this.showPasswordAlert = false
-
     this.showPasswordAlert = this.magicalUser.password != this.confirmPassword
   }
 
@@ -123,13 +114,55 @@ export class RegisterComponent implements OnInit {
    * @return true if all these cases are true
    */
   allFieldsAreEntered(): boolean {
-    let allFieldsAreEntered : boolean = this.privateUserInfo.address != null && this.privateUserInfo.email != null &&
-    this.privateUserInfo.lastName != null && this.privateUserInfo.phoneNumber != null &&
-    this.privateUserInfo.firstName != null && this.privateUserInfo.role != null &&
-    this.privateUserInfo.stateCity != null
+    let allFieldsAreEntered: boolean = this.privateUserInfo.address != null &&
+      this.privateUserInfo.email != null && this.privateUserInfo.lastName != null &&
+      this.privateUserInfo.phoneNumber != null && this.privateUserInfo.firstName != null &&
+      this.privateUserInfo.role != null && this.privateUserInfo.stateCity != null &&
+      this.magicalUser.password != null
+
+    //if the user registering is an adept they must provide a file of certification
+    if (this.privateUserInfo.role.id = 434)
+      allFieldsAreEntered = allFieldsAreEntered && this.magicalFileUpload != null
 
     this.fieldsAreBlank = !allFieldsAreEntered
 
     return allFieldsAreEntered
+  }
+
+  checkCityState(): void {
+    let city = this.citySelect.nativeElement.children[0].children[0].value
+    let state = this.stateSelect.nativeElement.children[0].children[0].value
+
+    if (city == null || state == null)
+      return
+
+    //check if the user inputted a valid state/city combination
+    this.codeListService.getStateCityCode(state, city).subscribe(
+      data => {
+        this.privateUserInfo.stateCity = data
+        this.showStateCityAlert = data == null
+      }
+    )
+  }
+
+  roleChange(): void {
+    if (this.privateUserInfo.role != null)
+      this.adeptAccount = this.privateUserInfo.role.id == 434
+  }
+
+  fileChange(event): void {
+    let fileBrowser = this.fileUpload.nativeElement;
+    if (fileBrowser.files && fileBrowser.files[0]) {
+      const formData = new FormData();
+      formData.append("image", fileBrowser.files[0]);
+      // this.http .post(`${this.apiEndPoint
+      // }`, formData, options)
+      //   .map(res => res.json())
+      //   .catch(error => Observable.throw(error))
+      //   .subscribe(
+      //   data => console.log('success'),
+      //   error => console.log(error)
+      //   )
+    }
   }
 }
