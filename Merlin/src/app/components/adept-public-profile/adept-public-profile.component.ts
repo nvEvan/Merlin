020916fa5f1adccba868bda3a877d62314c3ec89
Evan West <evan-west@hotmail.com>
2017-12-30 14:05:56 -0500

    Navigate by routing

--- Merlin/src/app/components/adept-public-profile/adept-public-profile.component.ts
+++ Merlin/src/app/components/adept-public-profile/adept-public-profile.component.ts
@@ -3,15 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-adept-public-profile',
   templateUrl: './adept-public-profile.component.html',
-  styleUrls: ['./adept-public-profile.component.css']
+  styleUrls: ['./adept-public-profile.component.css'],
+  inputs: ['Adept']
 })
 export class AdeptPublicProfileComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
-
-  navigateToAdeptPublicProfile() {
-    console.log("Hello!");
   }
 }
